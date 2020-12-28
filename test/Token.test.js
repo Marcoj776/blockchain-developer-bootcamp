64f266b2b3474fd25cb831053b57e0aa32523c81
Marco <marcojones1594@gmail.com>
2020-12-28 14:15:49 +0100

    tests pass

--- test/Token.test.js
+++ test/Token.test.js
@@ -7,11 +7,34 @@ const Token = artifacts.require('./Token')
 console.log(artifacts);
 
 contract('Token', (accounts) => {
+    let token
+    const settings = {
+        name: 'My Name',
+        symbol: 'DAPP',
+        decimals: '18',
+        totalSupply: '1' + ('0'.repeat(24)),
+    }
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
+
     describe('deployment', () => {
         it('tracks the name', async () => {
-            const token = await Token.new()
-            const result = await token.name()
-            result.should.equal('My Name')
+            const r = await token.name()
+            r.should.equal(settings.name)
+        })
+        it('tracks the symbol', async () => {
+            const r = await token.symbol()
+            r.should.equal(settings.symbol)
+        })
+        it('tracks the decimals', async () => {
+            const r = await token.decimals()
+            r.toString().should.equal(settings.decimals)
+        })
+        it('tracks the totalSupply', async () => {
+            const r = await token.totalSupply()
+            r.toString().should.equal(settings.totalSupply)
         })
     })
 })
