cca4ffe0ca3d1e31da5c73bf362a4e039fe9a0f2
Marcoj776 <marcojones1594@gmail.com>
2022-05-04 19:05:10 +0200

    Token transfers

--- truffle-config.js
+++ truffle-config.js
@@ -5,20 +5,19 @@ require('dotenv').config();
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
-
   compilers: {
     solc: {
       optimizer: {
-         enabled: true,
-         runs: 200
-      },
+        enabled: true,
+        runs: 200
+      }
     }
   }
 }
